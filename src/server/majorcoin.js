fc0216604a92a7035d5170a3048928c7e93e8240
Shivam Tripathi <shivam.flash@gmail.com>
2018-01-30 16:52:10 +0530

    Set up

---
+++ src/server/majorcoin.js
@@ -0,0 +1,9 @@
+import koa from 'koa';
+import Blockchain from './blockchain';
+import Node from './node';
+
+var app = koa();
+
+export function majorcoin(host, port, peers) {
+
+}
---
+++ test/server/majorcoin.js
@@ -0,0 +1,24 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.majorcoin = majorcoin;
+
+var _koa = require('koa');
+
+var _koa2 = _interopRequireDefault(_koa);
+
+var _blockchain = require('./blockchain');
+
+var _blockchain2 = _interopRequireDefault(_blockchain);
+
+var _node = require('./node');
+
+var _node2 = _interopRequireDefault(_node);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _koa2.default)();
+
+function majorcoin(host, port, peers) {}
