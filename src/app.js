9994fb092cb59b170c764079a57f035be03c4ff1
Shivam Tripathi <shivamtripathi0108@gmail.com>
2018-04-18 02:11:12 +0530

    Modify src/app to include all new routes and setup basic express server

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
 #!/usr/bin/env node
-import HttpServer from './httpserver';
+import config from './utils/config';
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import initRoutes from './routes/__init__';
 
-let httpServer = new HttpServer();
-httpServer.run();
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+initRoutes(app);
+
+app.listen(config.server.port, config.server.host, () => {
+	console.log(
+		`MajorCoin started on ${config.server.host}:${config.server.port}`
+	);
+});
